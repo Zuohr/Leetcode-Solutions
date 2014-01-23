fe8c17dadf38156bb399d18eba31d85a24dfb4e8
Zuohr <haoran.zuo@gmail.com>
2014-01-22 22:05:54 -0500

    test

--- LeetCode/src/test.java
+++ LeetCode/src/test.java
@@ -1,7 +1,21 @@
 
 public class test {
 	public static void main(String[] args) {
-
+		Test1 t = new Test1();
+		System.out.println(t.getNum());
 	}
+
+}
+
+class Test1 {
+	public int getNum() {
+		return num;
+	}
+
+	public void setNum(int num) {
+		this.num = num;
+	}
+
+	private int num;
 
 }
