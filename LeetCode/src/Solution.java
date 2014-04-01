0b74127da57bec902fed8eab4ce6ea954f414166
Zuohr <haoran.zuo@gmail.com>
2014-04-01 03:55:04 -0400

    update

--- LeetCode/src/Solution.java
+++ LeetCode/src/Solution.java
@@ -2959,70 +2959,57 @@ public class Solution {
 	 * end element.
 	 */
 	public int[] searchRange(int[] A, int target) {
-		if (A == null || A.length == 0 || target < A[0]
-				|| target > A[A.length - 1]) {
-			return new int[] { -1, -1 };
+		int[] result = { -1, -1 };
+		if (A == null) {
+			return result;
 		}
-		int len = A.length;
-		int l = 0, r = len - 1;
-		while (l <= r) {
-			int mid = (l + r) / 2;
+
+		result[0] = findStart(A, target);
+		if (result[0] != -1) {
+			result[1] = findEnd(A, target);
+		}
+
+		return result;
+	}
+
+	private int findStart(int[] A, int target) {
+		int start = 0, end = A.length - 1;
+		while (start <= end) {
+			int mid = start + (end - start) / 2;
 			if (A[mid] == target) {
-				break;
-			} else if (A[mid] > target) {
-				r = mid - 1;
-			} else {
-				l = mid + 1;
-			}
-		}
-		if (l > r) {
-			return new int[] { -1, -1 };
-		} else if (l == r) {
-			return new int[] { l, r };
-		} else {
-			int anchor = (l + r) / 2;
-			int start = l, end = anchor;
-			int[] pair = new int[2];
-			while (start <= end) {
-				int mid = (start + end) / 2;
-				if (A[mid] == target) {
-					if (mid == l) {
-						pair[0] = mid;
-						break;
-					} else {
-						if (A[mid - 1] == A[mid]) {
-							end = mid - 1;
-						} else {
-							pair[0] = mid;
-							break;
-						}
-					}
-				} else {
-					start = mid + 1;
-				}
-			}
-			start = anchor;
-			end = r;
-			while (start <= end) {
-				int mid = (start + end) / 2;
-				if (A[mid] == target) {
-					if (mid == r) {
-						pair[1] = mid;
-						break;
-					} else {
-						if (A[mid + 1] == A[mid]) {
-							start = mid + 1;
-						} else {
-							pair[1] = mid;
-							break;
-						}
-					}
+				if (mid == 0 || A[mid - 1] != target) {
+					return mid;
 				} else {
 					end = mid - 1;
 				}
+			} else if (A[mid] < target) {
+				start = mid + 1;
+			} else {
+				end = mid - 1;
 			}
-			return pair;
 		}
+
+		return -1;
+	}
+
+	private int findEnd(int[] A, int target) {
+		int start = 0, end = A.length - 1;
+		while (start <= end) {
+			int mid = start + (end - start) / 2;
+			if (A[mid] == target) {
+				if (mid == A.length - 1 || A[mid + 1] != target) {
+					return mid;
+				} else {
+					start = mid + 1;
+				}
+			} else if (A[mid] < target) {
+				start = mid + 1;
+			} else {
+				end = mid - 1;
+			}
+		}
+
+		return -1;
 	}
 
 	/**
